99fcc266744a7aeca2127489fd80b66a3293417a
SpudTatterson <tchcjhui123@gmail.com>
2023-08-07 13:12:16 +0300

    Adds Aim Assist

--- Assets/Scripts/GrapplingGun.cs
+++ Assets/Scripts/GrapplingGun.cs
@@ -7,6 +7,7 @@ public class GrapplingGun : MonoBehaviour
     [SerializeField] float maxDistance = 100f;
     [SerializeField] float swingForce = 10f;
     [SerializeField] float releaseDistance = 2f;
+    [SerializeField] float aimAssistRadius = 2f;
 
     LineRenderer lr;
     Vector3 grapplePoint;
@@ -48,41 +49,99 @@ public class GrapplingGun : MonoBehaviour
         }
     }
 
+    Vector3 CheckForSwingPoint()
+    {
+        RaycastHit rayHit;
+        RaycastHit sphereHit;
+        Vector3 SwingPoint;
+
+        Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(camRay, out rayHit, maxDistance, grappleable))
+        {
+            SwingPoint = rayHit.point;
+            return SwingPoint;
+        }
+        if(Physics.SphereCast(camRay, aimAssistRadius,out sphereHit, maxDistance, grappleable))
+        {
+            SwingPoint = sphereHit.point;
+            return SwingPoint;
+        }
+        // return blank if no potential swinging points are found
+        return Vector3.zero;
+    }
+
     void StartGrapple()
     {
-        rb.useGravity = true;
-        RaycastHit hit;
-        Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(camRay, out hit, maxDistance, grappleable))
+        grapplePoint = CheckForSwingPoint();
+        if(grapplePoint != Vector3.zero)
         {
+            rb.useGravity = true;
             lr.enabled = true;
-            pm.swinging = true; 
+            pm.swinging = true;
 
-            grapplePoint = hit.point;
-            
             joint = rb.gameObject.AddComponent<SpringJoint>();
-            joint.autoConfigureConnectedAnchor = false;
-            joint.connectedAnchor = grapplePoint;
+            ConfigureJoint();
 
-            float distanceFromPoint = Vector3.Distance(pm.transform.position, grapplePoint);
-
-            Debug.Log(distanceFromPoint);
-
-            // the distance grapple will try to keep from grapple point. 
-            joint.maxDistance = distanceFromPoint * 0.8f;
-            joint.minDistance = distanceFromPoint * 0.3f;
-
-            // customize values as you like
-            joint.spring = 4.5f;
-            joint.damper = 7f;
-            joint.massScale = 4.5f;
-
-            // swingDirection = (grapplePoint - transform.position).normalized;
-            // swingDistance = Vector3.Distance(transform.position, grapplePoint);
             isGrappling = true;
         }
-        
+
+
     }
+
+    private void ConfigureJoint()
+    {
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = grapplePoint;
+
+        float distanceFromPoint = Vector3.Distance(pm.transform.position, grapplePoint);
+
+        Debug.Log(distanceFromPoint);
+
+        // the distance grapple will try to keep from grapple point. 
+        joint.maxDistance = distanceFromPoint * 0.8f;
+        joint.minDistance = distanceFromPoint * 0.3f;
+
+        joint.spring = 4.5f;
+        joint.damper = 7f;
+        joint.massScale = 4.5f;
+    }
+
+    // void StartGrapple()
+    // {
+    //     rb.useGravity = true;
+    //     RaycastHit hit;
+    //     Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+    //     if (Physics.Raycast(camRay, out hit, maxDistance, grappleable))
+    //     {
+    //         lr.enabled = true;
+    //         pm.swinging = true; 
+
+    //         grapplePoint = hit.point;
+            
+    //         joint = rb.gameObject.AddComponent<SpringJoint>();
+    //         joint.autoConfigureConnectedAnchor = false;
+    //         joint.connectedAnchor = grapplePoint;
+
+    //         float distanceFromPoint = Vector3.Distance(pm.transform.position, grapplePoint);
+
+    //         Debug.Log(distanceFromPoint);
+
+    //         // the distance grapple will try to keep from grapple point. 
+    //         joint.maxDistance = distanceFromPoint * 0.8f;
+    //         joint.minDistance = distanceFromPoint * 0.3f;
+
+    //         // customize values as you like
+    //         joint.spring = 4.5f;
+    //         joint.damper = 7f;
+    //         joint.massScale = 4.5f;
+
+    //         // swingDirection = (grapplePoint - transform.position).normalized;
+    //         // swingDistance = Vector3.Distance(transform.position, grapplePoint);
+    //         isGrappling = true;
+    //     }
+        
+    // }
 
     // void UpdateSwingPosition()
     // {
