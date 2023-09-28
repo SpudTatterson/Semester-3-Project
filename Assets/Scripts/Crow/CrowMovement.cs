16ec43d6e0f8ba51e1f4ee4fd503063952e29990
SpudTatterson <tchcjhui123@gmail.com>
2023-09-28 19:03:47 +0300

    Adds Ring events

--- Assets/Scripts/Crow/CrowMovement.cs
+++ Assets/Scripts/Crow/CrowMovement.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CrowMovement : MonoBehaviour
 {
     [Header("Destinations")]
     [SerializeField] CrowDestination[] crowDestinations;
+    [Header("Crow Actions")]
+    [SerializeField] int ringLocation;
+    [SerializeField] UnityEvent ringPickUpEvents;   
 
 
     int currentLocation = 0;
@@ -14,7 +18,7 @@ public class CrowMovement : MonoBehaviour
     [Header("References")]
     Transform player;
     Animator animator;
-    
+
 
     void Awake()
     {
@@ -23,46 +27,50 @@ public class CrowMovement : MonoBehaviour
         transform.position = crowDestinations[0].Destination;
     }
     IEnumerator GoToNextLocation()
-{
-    if (currentLocation + 1 >= crowDestinations.Length)
     {
-        // Got to crow this is the win condition so trigger win thing here i guess
-        yield break;
+        if (currentLocation + 1 >= crowDestinations.Length)
+        {
+            // Got to crow this is the win condition so trigger win thing here i guess
+            yield break;
+        }
+        startedFlying = true;
+        animator.SetBool("Flying", true);
+        animator.SetBool("Eating", false);
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = crowDestinations[currentLocation + 1].Destination;
+        transform.rotation = Quaternion.LookRotation(targetPosition);
+        float journeyLength = Vector3.Distance(startPosition, targetPosition);
+        float journeyTime = crowDestinations[currentLocation + 1].time;
+
+        float startTime = Time.time;
+
+        while (Time.time < startTime + journeyTime)
+        {
+            float distanceCovered = (Time.time - startTime) * journeyLength / journeyTime;
+            float fractionOfJourney = distanceCovered / journeyLength;
+
+            transform.position = Vector3.Lerp(startPosition, targetPosition, fractionOfJourney);
+
+            yield return null;
+        }
+        transform.position = targetPosition;
+        currentLocation++;
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        animator.SetBool("Flying", false);
+        startedFlying = false;
     }
-    startedFlying = true;
-    animator.SetBool("Flying", true);
 
-    Vector3 startPosition = transform.position;
-    Vector3 targetPosition = crowDestinations[currentLocation + 1].Destination;
-    transform.rotation = Quaternion.LookRotation(targetPosition);
-    float journeyLength = Vector3.Distance(startPosition, targetPosition);
-    float journeyTime = crowDestinations[currentLocation + 1 ].time;
 
-    float startTime = Time.time;
-
-    while (Time.time < startTime + journeyTime)
+    void Update()
     {
-        float distanceCovered = (Time.time - startTime) * journeyLength / journeyTime;
-        float fractionOfJourney = distanceCovered / journeyLength;
-        
-        transform.position = Vector3.Lerp(startPosition, targetPosition, fractionOfJourney);
-
-        Debug.Log("flying");
-        yield return null;
-    }
-    Debug.Log("finished flying");
-    transform.position = targetPosition;
-    currentLocation++;
-    animator.SetBool("Flying", false);
-    startedFlying = false;
-}
-
-
-    void Update() 
-    {
-        if((Vector3.Distance(player.position, transform.position) < crowDestinations[currentLocation].distance) && !startedFlying)
+        if(currentLocation == ringLocation)
+        {
+            transform.rotation = Quaternion.Euler(0, 328.259f,0);
+            ringPickUpEvents.Invoke();
+        }
+        if ((Vector3.Distance(player.position, transform.position) < crowDestinations[currentLocation].distance) && !startedFlying)
         {
             StartCoroutine(GoToNextLocation());
-        }    
+        }
     }
 }
