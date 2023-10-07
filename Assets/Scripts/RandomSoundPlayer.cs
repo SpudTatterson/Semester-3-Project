a2081e4418c7714f25df56dd468bb5b55516ce5c
SpudTatterson <tchcjhui123@gmail.com>
2023-10-07 10:36:20 +0300

    Added Random sound player

---
+++ Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomSoundPlayer : MonoBehaviour
+{
+    [SerializeField] Vector2 minMaxTimeBetweenGames;
+    [Header("References")]
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] clips;
+
+
+    bool started = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            started = true;
+            float time = Random.Range(minMaxTimeBetweenGames.x, minMaxTimeBetweenGames.y);
+            Invoke("Play", time);
+        }
+
+    }
+
+    void Play()
+    {
+        SetRandomClip();
+        audioSource.Play();
+        Invoke("ToggleStarted" ,audioSource.clip.length);
+    }
+    void ToggleStarted()
+    {
+        started = false;
+    }
+    void SetRandomClip()
+    {
+        int clipIndex = Random.Range(0, clips.Length);
+        audioSource.clip = clips[clipIndex];
+    }
+}
